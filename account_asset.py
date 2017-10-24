001723169dc6901c2293dc088030d493fa9e89a6
Giuseppe D'Alò <g.dalo@cgsoftware.it>
2017-10-24 18:33:09 +0200

    Migliorato il calcolo del residuo sul cespite

--- account_asset.py
+++ account_asset.py
@@ -111,7 +111,7 @@ class account_asset_asset(orm.Model):
             result[asset.id]['remaining_value'] = (
                 asset.value_residual - result[asset.id][
                     'accumulated_depreciation']) + asset.incremental_value + \
-                     asset.gains-asset.decremental_value-asset.losses
+                     asset.gains-asset.decremental_value-asset.losses-asset.sale_value
         return result
 
     _columns = {
